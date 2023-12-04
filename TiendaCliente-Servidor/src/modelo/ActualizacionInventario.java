78594f6e805948a231a8f2e00edb8b1d2995c9e9
Esteban Sanarrucia <esteban9926@hotmail.com>
2023-12-03 23:43:49 -0600

    Revert "Update ActualizacionInventario.java"
    This reverts commit 67202950e3dfd014021132b4e20958c3d9f918ec.

--- TiendaCliente-Servidor/src/modelo/ActualizacionInventario.java
+++ TiendaCliente-Servidor/src/modelo/ActualizacionInventario.java
@@ -1,22 +1,18 @@
 package modelo;
 
-import java.sql.PreparedStatement;
-import javax.swing.JOptionPane;
-import vista.VentasForm;
 import java.sql.SQLException;
-import modelo.ConexionBD;
 
 public class ActualizacionInventario {
-
+  
     private ConexionBD conexionBD;
-    ConexionBD conexion = new ConexionBD("root", "admin");
-    
+  
     public ActualizacionInventario(ConexionBD conexionBD) {
         this.conexionBD = conexionBD;
     }
-
+  
     public void actualizarInventario(int idProducto, int cantidadVendida) {
         try {
+            // Actualiza la cantidad en el inventario
             conexionBD.setConsulta("UPDATE inventario SET cantidad = cantidad - ? WHERE id_producto = ?");
             conexionBD.getConsulta().setInt(1, cantidadVendida);
             conexionBD.getConsulta().setInt(2, idProducto);
