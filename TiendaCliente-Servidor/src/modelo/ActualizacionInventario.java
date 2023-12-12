81038de243624449bf6f72ce061d9a9647108134
Alesaenzz <133941556+Alesaenzz@users.noreply.github.com>
2023-12-12 05:25:03 -0600

    Update ActualizacionInventario.java

--- TiendaCliente-Servidor/src/modelo/ActualizacionInventario.java
+++ TiendaCliente-Servidor/src/modelo/ActualizacionInventario.java
@@ -17,6 +17,12 @@ public class ActualizacionInventario {
             conexionBD.getConsulta().setInt(1, cantidadVendida);
             conexionBD.getConsulta().setInt(2, idProducto);
             conexionBD.getConsulta().executeUpdate();
+          
+            // Registra la venta en la tabla ventas
+            conexionBD.setConsulta("INSERT INTO ventas (idProducto, cantidadProducto) VALUES (?, ?)");
+            conexionBD.getConsulta().setInt(1, idProducto);
+            conexionBD.getConsulta().setInt(2, cantidadVendida);
+            conexionBD.getConsulta().executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
